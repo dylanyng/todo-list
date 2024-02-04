974d9ab07c6118fad2ccb8d1ffa63ec6ac4f7a81
dylanyng <dylanyoung@hey.com>
2024-02-04 17:11:29 -0500

    Update the session code to be in line with the new MongoStore requirements.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
+const MongoStore = require('connect-mongo')
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
@@ -26,12 +26,14 @@ app.use(logger('dev'))
 // Sessions
 app.use(
     session({
-      secret: 'keyboard cat',
-      resave: false,
-      saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ 
+        mongoUrl: mongoose.connection.getClient().s.url
+        })
     })
-  )
+);
 
 // Passport middleware
 app.use(passport.initialize())
